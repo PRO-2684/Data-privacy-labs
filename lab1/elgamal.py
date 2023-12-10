b17ac44ffee6131acf937e26c5ca59190db2af78
PRO-2684 <54608551+PRO-2684@users.noreply.github.com>
2023-12-10 11:51:36 +0800

    Format; Ignore `__pycache__`

--- lab1/elgamal.py
+++ lab1/elgamal.py
@@ -6,7 +6,7 @@ DEFAULT_KEY_SIZE = 256
 
 
 def is_primitive_root(g: int, p: int, factors: dict) -> bool:
-    # determine whether g is a primitive root of p
+    """Determine whether g is a primitive root of p"""
     for factor in factors:
         if pow(g, (p - 1) // factor, p) == 1:
             return False
@@ -14,33 +14,33 @@ def is_primitive_root(g: int, p: int, factors: dict) -> bool:
 
 
 def generate_p_and_g(n_bit: int) -> (int, int):
+    """Generate a large prime number p and its primitive root g."""
     while True:
-        # generate an n-bit random prime number p
-        p = sympy.randprime(2 ** (n_bit - 1), 2**n_bit)
-
-        # compute the prime factorization of p-1
-        factors = sympy.factorint(p - 1).keys()
-
-        # choose a possible primitive root g
+        p = sympy.randprime(
+            2 ** (n_bit - 1), 2**n_bit
+        )  # Generate an n-bit random prime number p
+        factors = sympy.factorint(
+            p - 1
+        ).keys()  # Compute the prime factorization of p-1
+        # Choose a possible primitive root g
         for g in range(2, p):
             if is_primitive_root(g, p, factors):
-                return p, g
+                return p, g  # Found primitive root g
 
 
 def mod_exp(base: int, exponent: int, modulus: int) -> int:
-    """DONE: calculate (base^exponent) mod modulus.
-    Recommend to use the fast power algorithm.
-    """
+    """Calculate (base^exponent) mod modulus using the fast power algorithm."""
     result = pow(base, exponent, modulus)
     return result
 
 
 def elgamal_key_generation(key_size: int) -> ((int, int, int), int):
     """Generate the keys based on the key_size."""
-    # generate a large prime number p and a primitive root g
-    p, g = generate_p_and_g(key_size)
+    p, g = generate_p_and_g(
+        key_size
+    )  # Generate a large prime number p and a primitive root g
 
-    # FIXME: generate private key x and public key y here.
+    # Generate private key x and public key y.
     x = random.randint(1, p - 2)
     y = mod_exp(g, x, p)
 
@@ -48,45 +48,41 @@ def elgamal_key_generation(key_size: int) -> ((int, int, int), int):
 
 
 def elgamal_encrypt(public_key: (int, int, int), plaintext: int) -> (int, int):
-    """FIXME: encrypt the plaintext with the public key."""
+    """Encrypt the plaintext with the public key."""
     p, g, y = public_key
-    # generate a random number k (temporary private key such that 1 <= k <= p-2)
-    k = random.randint(1, p - 2)
-    # compute temporary public key c1
-    c1 = mod_exp(g, k, p)
-    # compute encrypted message c2
-    c2 = plaintext * mod_exp(y, k, p) % p
+    k = random.randint(1, p - 2)  # Generate a temporary private key k (1 <= k <= p-2)
+    c1 = mod_exp(g, k, p)  # Compute temporary public key c1
+    c2 = plaintext * mod_exp(y, k, p) % p  # Compute encrypted message c2
     return c1, c2
 
 
-def elgamal_decrypt(public_key: (int, int, int), private_key: int, ciphertext: (int, int)) -> int:
-    """FIXME: decrypt the ciphertext with the public key and the private key."""
+def elgamal_decrypt(
+    public_key: (int, int, int), private_key: int, ciphertext: (int, int)
+) -> int:
+    """DONE: decrypt the ciphertext with the public key and the private key."""
     p, g, y = public_key
     c1, c2 = ciphertext
-    # compute the inverse of c1 s
-    s = mod_exp(c1, private_key, p)
-    # compute the inverse of s s_
-    s_ = sympy.core.numbers.mod_inverse(s, p)
-    # compute the plaintext
-    plaintext = c2 * s_ % p
+    s = mod_exp(c1, private_key, p)  # Compute the inverse of c1 s
+    s_ = sympy.core.numbers.mod_inverse(s, p)  # Compute the inverse of s, s_
+    plaintext = c2 * s_ % p  # Compute the plaintext
     return plaintext
 
 
 if __name__ == "__main__":
-    # set key_size, such as 256, 1024...
+    # Set key_size, such as 256, 1024...
     key_size = input("Please input the key size: ")
     key_size = int(key_size) if key_size else DEFAULT_KEY_SIZE
 
-    # generate keys
+    # Generate keys
     public_key, private_key = elgamal_key_generation(key_size)
     print("Public Key:", public_key)
     print("Private Key:", private_key)
 
-    # encrypt plaintext
+    # Encrypt plaintext
     plaintext = int(input("Please input an integer: "))
     ciphertext = elgamal_encrypt(public_key, plaintext)
     print("Ciphertext:", ciphertext)
 
-    # decrypt ciphertext
+    # Decrypt ciphertext
     decrypted_text = elgamal_decrypt(public_key, private_key, ciphertext)
     print("Decrypted Text:", decrypted_text)
